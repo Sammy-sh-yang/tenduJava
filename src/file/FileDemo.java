7629f56d7e264ccb239bd135cea33cdde5613a47
Sammyland000 <sammyland000@gmail.com>
2023-06-13 15:18:40 +0800

    New file

---
+++ src/file/FileDemo.java
@@ -0,0 +1,12 @@
+package file;
+
+import java.io.File;
+import java.util.Scanner;
+
+
+public class FileDemo {
+    public static void main(String[] args) {
+
+        File file = new File("");
+    }
+}
