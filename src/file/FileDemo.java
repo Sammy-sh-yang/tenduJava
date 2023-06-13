a182d96c28b8441b6330300d6528f9f6c245e780
Sammyland000 <sammyland000@gmail.com>
2023-06-13 15:39:55 +0800

    Add new file address

--- src/file/FileDemo.java
+++ src/file/FileDemo.java
@@ -7,6 +7,10 @@ import java.util.Scanner;
 public class FileDemo {
     public static void main(String[] args) {
 
-        File file = new File("");
+
+//        ./a/b/c/hello.txt
+        String filename = "./txt/txtFile.txt";
+        File file = new File(filename);
+
     }
 }
