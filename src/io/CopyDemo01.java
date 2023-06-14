d171688cf3970d8a049a09f707256186bf20a6ba
Sammyland000 <sammyland000@gmail.com>
2023-06-14 14:43:20 +0800

    Add new file address

--- src/io/CopyDemo01.java
+++ src/io/CopyDemo01.java
@@ -7,19 +7,20 @@ public class CopyDemo01 {
 
         long start = System.currentTimeMillis();
 
-
         File pic = new File("./txt/demo/02.png");
         File outPic = new File("./txt/demo/02-1.png");
 
         FileInputStream fis = new FileInputStream(pic);
         FileOutputStream fos = new FileOutputStream(outPic);
 
+        System.out.println("Size: "+ fis.available());
+
         int temp;
         while ((temp = fis.read()) != -1){
             fos.write(temp);
         }
         long end = System.currentTimeMillis();
-        System.out.println(end - start);
+        System.out.println((end - start) + "ms");
 
         fos.close();
         fis.close();
