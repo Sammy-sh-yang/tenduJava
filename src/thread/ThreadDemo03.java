5d2588dd0ff76b6074b7dda48f6dc843a1f54e2f
Sammyland000 <sammyland000@gmail.com>
2023-06-27 11:46:19 +0800

    2 method for Thread

---
+++ src/thread/ThreadDemo03.java
@@ -0,0 +1,43 @@
+package thread;
+
+public class ThreadDemo03 {
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    System.out.println("Hello");
+                }
+            }
+        };
+
+
+
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    System.out.println("World");
+                }
+            }
+        };
+
+
+
+        //use lambda method
+
+        Runnable r2 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("kekekekek");
+            }
+        };
+
+        t1.start();
+        Thread t2 = new Thread(r1);
+        t2.start();
+        Thread t3 = new Thread(r2);
+        t3.start();
+
+    }
+}
