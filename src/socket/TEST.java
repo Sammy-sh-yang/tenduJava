72b9610ddbaecb0424889bdf082ceac7ab511b50
Sammyland000 <sammyland000@gmail.com>
2023-06-29 17:00:54 +0800

    Socket client server

--- src/socket/TEST.java
+++ src/socket/TEST.java
@@ -3,50 +3,46 @@ package socket;
 import java.util.Random;
 
 public class TEST {
-
     public static void main(String[] args) {
 
         TEST t = new TEST();
-
         try {
             t.eat();
-        } catch (WrongName wrongName) {
-            wrongName.printStackTrace();
+        } catch (NumberErrorException e) {
+            e.printStackTrace();
         }
-
     }
 
-    public void eat() throws WrongName {
+    public void eat() throws NumberErrorException{
         Random rand = new Random();
-        int x = rand.nextInt(2);
-        if (x == 0) {
-            throw new WrongName("Bro it's 0");
+        int num = rand.nextInt(2);
+        if (num == 0) {
+            throw new NumberErrorException("The number cannot be 0");
         } else {
-            System.out.println(x);
+            System.out.println(num);
         }
-
     }
 
 }
 
-
-class WrongName extends Exception {
-    public WrongName() {
+class NumberErrorException extends Exception{
+    public NumberErrorException() {
     }
 
-    public WrongName(String message) {
+    public NumberErrorException(String message) {
         super(message);
     }
 
-    public WrongName(String message, Throwable cause) {
+    public NumberErrorException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public WrongName(Throwable cause) {
+    public NumberErrorException(Throwable cause) {
         super(cause);
     }
 
-    public WrongName(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    public NumberErrorException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
+
